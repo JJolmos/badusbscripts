2ef13a1c6999da7caac60d9ed5a60cb3db668c3e
Juan José Olmos Manríquez <olmos_jj@hotmail.com>
2024-12-12 09:19:37 -0600

    Update main.ps1

--- w10/main.ps1
+++ w10/main.ps1
@@ -22,7 +22,7 @@ $usrimg = "$image"
 # HTML FOR COVER PAGE
 $h = @"
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
 	  <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
